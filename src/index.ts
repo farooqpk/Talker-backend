9052f3efa9ccc32562b7e73206244a5f9de886c3
Ummar Farooq <farooqcherur@gmail.com>
2023-05-31 20:54:12 +0530

    designed MVC folder structure and configured router and cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, urlencoded } from "express";
+import {router} from './routes/route'
+import cors from 'cors'
 
 const app: Express = express();
+
+app.use("/",router)
+app.use(cors())
+app.use(express.json())
+app.use(urlencoded({extended:false}))
+
 
 app.get("/", (req: Request, res: Response): void => {
   res.send("hello from server");
 });
+
 
 app.listen(3000, (): void => {
   console.log(`server listening @ http://localhost:3000/`);
---
+++ src/routes/route.ts
@@ -0,0 +1,8 @@
+
+import Express, { Router } from 'express'
+
+export const router:Router = Express.Router()
+
+router.post('/login')
+
+
