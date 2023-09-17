5a2cce91d57e6df0edbb136cd39347893aefb1ed
Ummar Farooq <farooqcherur@gmail.com>
2023-09-17 14:32:50 +0530

    bug fix

--- src/socket/socketHandler.ts
+++ src/socket/socketHandler.ts
@@ -2,7 +2,6 @@ import { Server, Socket } from "socket.io";
 import { ChatMsg } from "../types/ChatMsgType";
 import { getSocketsFromRedis } from "../redis/getSockets";
 import { DecodedPayload } from "../types/DecodedPayload";
-import { ChatModel } from "../models/chat/ChatMsg";
 
 export const socketHandler = (
   socket: Socket,
@@ -20,26 +19,6 @@ export const socketHandler = (
       socket
         .to(recipientId as string)
         .emit("chat", { sender: decodedPayload.username, message });
-
-      await ChatModel.updateOne(
-        {
-          $or: [
-            { users: [decodedPayload.userId, recipient] },
-            { users: [recipient, decodedPayload.userId] },
-          ],
-        },
-        {
-          $push: {
-            messages: {
-              senderId: decodedPayload.userId,
-              recipientId: recipient,
-              textMsg: message,
-            },
-          },
-          users: [decodedPayload.userId, recipient],
-        },
-        { upsert: true }
-      );
     } else {
       socket.emit("offline", "now recipient is offline");
     }
