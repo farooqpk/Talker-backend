06bfcde9fe837f40297d9c6bfb531eeae2014425
farooqpk <farooqcherur@gmail.com>
2024-03-04 07:58:48 +0530

    chat list api

--- src/controllers/chat/chatList.ts
+++ src/controllers/chat/chatList.ts
@@ -2,5 +2,35 @@ import { Request, Response } from "express";
 import { prisma } from "../../utils/prisma";
 
 export const chatList = async (req: Request, res: Response) => {
- 
+  try {
+    const chats = await prisma.chat.findMany({
+      where: {
+        participants: {
+          some: {
+            userId: {
+              equals: req.userId,
+            },
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        messages: {
+          orderBy: {
+            createdAt: "desc",
+          },
+          take: 1,
+        },
+      },
+    });
+
+    res.status(200).json(chats);
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "there is an error while fetching chats",
+    });
+  }
 };
