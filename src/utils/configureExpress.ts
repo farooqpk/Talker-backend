0f034ca866c9542406c56edc1ba827abbeb6f09e
farooqpk <farooqcherur@gmail.com>
2024-07-14 17:21:54 +0530

    cors header change

--- src/utils/configureExpress.ts
+++ src/utils/configureExpress.ts
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 import { CLIENT_URL } from "../config";
 
 export function configureExpress(app: express.Express) {
-  app.use(cors({ origin: CLIENT_URL, credentials: true }));
+  app.use(cors({ origin: CLIENT_URL, credentials: true, allowedHeaders: ["*"] }));
   app.use(express.json());
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: true }));
