59d4b00feb8e572841a3cbf38a316d5e2f76fe78
farooqpk <farooqcherur@gmail.com>
2024-08-30 07:30:59 +0530

    build.sh script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Checkout frontend code
+git clone https://github.com/farooqpk/chatApp-frontEnd.git chatApp-frontEnd
+
+# Install frontend dependencies
+cd chatApp-frontEnd
+npm ci
+
+# Build frontend
+npm run build
+
+# Move frontend build to backend
+cd ..
+rm -rf ./client
+mv chatApp-frontEnd/dist ./client
+
+# Clean up
+rm -rf chatApp-frontEnd
